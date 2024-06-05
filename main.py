2e09e03fa6f16a4e646e323640ddac69df0d5d48
HaneeshDev <malepatihaneesh@gmail.com>
2024-06-05 23:59:55 +0530

    Scrapped info of temp and degree

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ url = f'https://www.google.com/search?q=weather+{query}'
 
 r = s.get(url,headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'})
 
-print(r.html.find('title'))
+temp = r.html.find('span#wob_tm',first=True).text
+
+print(temp)
+
+print(r.html.find("div.vk_bk.wob-unit",first=True))
